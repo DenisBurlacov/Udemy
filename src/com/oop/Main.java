3ca1be811aa27fd707caa536cb00ecacd0169f2b
Burlakov Denys <denis055023@gmail.com>
2021-10-03 06:41:18 +0300

    add value to the main

--- src/com/oop/Main.java
+++ src/com/oop/Main.java
@@ -5,16 +5,20 @@ import javax.sound.midi.Soundbank;
 public class Main {
     public static void main(String[] args) {
         Dog lab = new Dog();
-        lab.setPaws(5);
-        lab.setTail(2);
+        lab.setPaws(4);
+        lab.setTail(1);
         lab.setName("Charly");
         lab.setBreed("Lab");
+        lab.setSize("Average");
+        lab.barks();
 
         Dog sheppard = new Dog();
         sheppard.setPaws(4);
         sheppard.setTail(1);
         sheppard.setName("Mike");
         sheppard.setBreed("Sheppard");
+        sheppard.setSize("Big");
+        sheppard.barks();
 
         System.out.println("Lab's name is " + lab.getName());
         System.out.println("Sheppard's name is " + sheppard.getName());
